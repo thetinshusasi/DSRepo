63fad8759a8e0ce400b70f0472e94d63be6defb7
Tinshu Sasi <thetinshusasi@gmail.com>
2020-01-03 06:25:51 +0530

    data chapter 3

---
+++ DataStructures/DataStructures/Algo/Sorting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Algo
+{
+    public class Sorting
+    {
+        int[] InsertionSort(int[] arr)
+        {
+
+        }
+
+        int [] MergeSort(int[] arr)
+        {
+
+        }
+    }
+}
